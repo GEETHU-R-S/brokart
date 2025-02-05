7caa34392057af6110a4fa09ba56053a6048f1f3
Geethu R S <rsgeethu023@gmail.com>
2025-02-05 22:38:04 +0530

    Almost completed

--- brokart/orders/urls.py
+++ brokart/orders/urls.py
@@ -19,5 +19,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('cart',views.show_cart,name='cart'),
+    path('show_orders',views.show_orders,name='show_orders'),
+    path('add_to_cart',views.add_to_cart,name='add_to_cart'),
+    path('remove_item/<pk>',views.remove_item,name='remove_item'),
+    path('checkout_cart',views.checkout_cart,name='checkout_cart'),
 
 ]
